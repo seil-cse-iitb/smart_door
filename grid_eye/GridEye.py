a257c6e1d89e45cf660d90fb51e31433e3b11ca1
Sapan <sapantanted99@gmail.com>
2019-11-11 15:22:11 +0530

    single value moving average grideye, height sensor average of readings, and some other modifications

--- grid_eye/GridEye.py
+++ grid_eye/GridEye.py
@@ -2,6 +2,7 @@ from transitions import *
 import numpy as np
 from time import *
 from Adafruit_AMG88xx import *
+from queue import Queue
 import requests
 
 class GridEye(object):
@@ -24,21 +25,24 @@ class GridEye(object):
         self.status = "READING"
         self.prev_time = 0
         self.threshold_cross_count = 0
+        self.queue_length=20
+        self.queue=Queue(self.queue_length)
+        self.threshold_sum=0
         self.server_root=None
         # self.ts["READING", "TRIGGERED", "COMPLETED"] = [-1, -1, -1]
 
         print("Initialized Grid Eye")
 
-    def set_server_root(self,server_root):
-        self.server_root=server_root
+    def set_server_root(self, server_root):
+        self.server_root = server_root
 
-    def send_live_data(self,pixels):
+    def send_live_data(self, pixels):
         if self.server_root is None:
             return
-        url = self.server_root + '/ge_live_viz/'+requests.utils.quote(str(pixels))
+        url = self.server_root + '/ge_live_viz/' + requests.utils.quote(str(pixels))
         response = requests.get(url)
         if response.status_code != 200:
-            print("ge_live_data could not be sent! http response code: ",str(response.status_code))
+            print("ge_live_data could not be sent! http response code: ", str(response.status_code))
 
     def set_status(self, status):
         self.status = status
@@ -59,24 +63,37 @@ class GridEye(object):
 
     def set_threshold(self, pixels, threshold):
         current_threshold = int(np.mean(np.sort(pixels)[-5:]))
-        if -2 < (current_threshold - threshold) > 2:
+        if abs(current_threshold - threshold) > 2:
             if (int(time()) - self.prev_time) > 1:
-                if self.verbose:
-                    print("Crossed threshold : ", self.threshold_cross_count)
+                # if self.verbose:
                 self.prev_time = int(time())
                 self.threshold_cross_count += 1
-            else:
-                self.threshold_cross_count = 0
+                print("Crossed threshold : ", self.threshold_cross_count)
+        else:
+            self.threshold_cross_count = 0
+            # print("threshold count reset")
         if self.threshold_cross_count > 5:
-            if self.verbose:
-              print("Prev Threshold : %d \t Current Threshold : %d"%(threshold, current_threshold))
+            # if self.verbose:
+            print("Prev Threshold : %d \t Current Threshold : %d"%(threshold, current_threshold))
             threshold =  current_threshold
             self.threshold_cross_count = 0
         return threshold
 
+    def moving_average_threshold(self,pixels):
+        threshold = int(np.mean(np.sort(pixels)[-5:]))
+        if not self.queue.full():
+            self.queue.put(threshold)
+            self.threshold_sum+=threshold
+        else:
+            self.threshold_sum-=self.queue.get()
+            #self.queue = self.queue[1:]
+            self.queue.put(threshold)
+            self.threshold_sum+=threshold
+        return int(self.threshold_sum/self.queue_length)
+
     def monitor_ones(self):
         self.read_pixels()
-        self.threshold = int(np.mean(np.sort(self.pixels)[-5:]))
+        threshold_updated = int(np.mean(np.sort(self.pixels)[-5:]))
         first_direction = 0
         second_direction = 0
         self.set_status("READING")
@@ -87,76 +104,75 @@ class GridEye(object):
                 continue
 
             self.read_pixels()
-            if np.std(self.pixels)<2.6 and self.get_status() == "READING" :
-                self.threshold = int(np.mean(self.pixels))
-            else:
-                # self.threshold = self.set_threshold(self.pixels, self.threshold)
-                pixels_array = np.transpose(np.reshape(self.pixels, [8, 8]) > self.threshold + 1).astype(int)
-                right = pixels_array[:, 0:3]
-                left = pixels_array[:, -3:]
+            # threshold_updated = self.set_threshold(self.pixels, threshold_updated)
+            if self.get_status()=="READING" or not self.queue.full():
+                threshold_updated = self.moving_average_threshold(self.pixels)
+            # threshold_updated = 85
+            # pixels_array = np.transpose(np.reshape(self.pixels, [8, 8]) > threshold_updated + 1).astype(int)
+            pixels_array = (np.reshape(self.pixels, [8, 8]) > threshold_updated + 1).astype(int)
+            right = pixels_array[:, 0:3]
+            left = pixels_array[:, -3:]
 
-                all_count = np.count_nonzero(pixels_array[:, 1:])
-                right_count = np.count_nonzero(right)
-                left_count = np.count_nonzero(left)
+            all_count = np.count_nonzero(pixels_array[:, 1:])
+            right_count = np.count_nonzero(right)
+            left_count = np.count_nonzero(left)
 
-                self.send_live_data(pixels_array)
-                if self.verbose:
-                    print("Grid Eye Output:")
-                    print(pixels_array)
-                    # print(np.reshape(self.pixels, [8, 8]))
-                    print(np.std(self.pixels))
-                    print("Right count: %d & Left count: %d\n" % (np.count_nonzero(right), np.count_nonzero(left)))
-                # print("right:\n",pixels_array[:,0:3],"\nleft:\n",pixels_array[:,-3:],"\n")
+            self.send_live_data(pixels_array)
+            print("threshold: ",str(threshold_updated))
+            if self.verbose:
+                print("Grid Eye Output:")
+                print(pixels_array)
+                print("Right count: %d & Left count: %d\n" % (np.count_nonzero(right), np.count_nonzero(left)))
+            # print("right:\n",pixels_array[:,0:3],"\nleft:\n",pixels_array[:,-3:],"\n")
 
-                # print("All count", all_count)
-                # print("Left count", left_count)
-                # print("Right count", right_count)
-                # print("")
+            # print("All count", all_count)
+            # print("Left count", left_count)
+            # print("Right count", right_count)
+            # print("")
 
-                if all_count > 12:
-                    if first_direction == 0:
-                        if left_count - right_count > 5 or left_count - right_count < -5:
-                            first_direction = left_count - right_count
-                            self.set_status("TRIGGERED")
-                            print(self.threshold)
-                            # print("Set first_direction : ", first_direction)
-                    else:
-                        second_direction = left_count - right_count
-                        # print("Set second_direction", second_direction)
+            if all_count > 12:
+                if first_direction == 0:
+                    if left_count - right_count > 5 or left_count - right_count < -5:
+                        first_direction = left_count - right_count
+                        self.set_status("TRIGGERED")
+                        # print("Set first_direction : ", first_direction)
                 else:
-                    if first_direction < 0 and second_direction < 0:
-                        print("Bhag gaya :D")
-                        self.event = -2
-                        self.callback(self.event)
-                        self.set_status("COMPLETED")
-                    elif first_direction < 0 and second_direction > 0:
-                        print("Entry")
-                        self.event = 1
-                        self.callback(self.event)
-                        self.set_status("COMPLETED")
-                    elif first_direction > 0 and second_direction < 0:
-                        print("Exit")
-                        self.event = -1
-                        self.callback(self.event)
-                        self.set_status("COMPLETED")
-                    elif first_direction > 0 and second_direction > 0:
-                        print("Andar aa gaya :D")
-                        self.event = 2
-                        self.callback(self.event)
-                        self.set_status("COMPLETED")
-                    else:
-                        if self.get_status() == "TRIGGERED":
-                            self.event=None
-                            self.status="READING"
-                            # self.event = None
-                            # self.callback(self.event)  # Something wrong
-                            # self.set_status("COMPLETED")
-                            print("Something wrong:: first_direction: ", first_direction, ", second_direction:",
-                                second_direction)
-                    first_direction = 0
-                    second_direction = 0
+                    second_direction = left_count - right_count
+                    # print("Set second_direction", second_direction)
+            else:
+                if first_direction < 0 and second_direction < 0:
+                    print("Bhag gaya :D")
+                    self.event = -2
+                    self.callback(self.event)
+                    self.set_status("COMPLETED")
+                elif first_direction < 0 and second_direction > 0:
+                    print("Entry")
+                    self.event = 1
+                    self.callback(self.event)
+                    self.set_status("COMPLETED")
+                elif first_direction > 0 and second_direction < 0:
+                    print("Exit")
+                    self.event = -1
+                    self.callback(self.event)
+                    self.set_status("COMPLETED")
+                elif first_direction > 0 and second_direction > 0:
+                    print("Andar aa gaya :D")
+                    self.event = 2
+                    self.callback(self.event)
+                    self.set_status("COMPLETED")
+                else:
+                    if self.get_status() == "TRIGGERED":
+                        self.event=None
+                        self.status="READING"
+                        # self.event = None
+                        # self.callback(self.event)  # Something wrong
+                        # self.set_status("COMPLETED")
+                        print("Something wrong:: first_direction: ", first_direction, ", second_direction:",
+                              second_direction)
+                first_direction = 0
+                second_direction = 0
 
-                sleep(0.05)
+            sleep(0.05)
 
     def reset_status_and_data(self):
         self.event=None
